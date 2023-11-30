9c99f33bb0ef162e775a316bf26084df21a0dc9e
Ziad El-Fakhrany <ziad.fakhranyy@gmail.com>
2023-11-30 19:24:37 +0200

    database config & connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReadersModule } from './readers/readers.module';
+import { AdminsModule } from './admins/admins.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    ReadersModule,
+    AdminsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
